32f150ace07ab30e12e6ae8ac3adc7a4ed84b34d
Marcelo Castelo Branco Alves da Silva <marcelo.castelo@outlook.com>
2022-04-06 01:47:45 -0300

    feat: add DomainEntityBase

---
+++ src/MCB.Core.Domain.Entities/DomainEntityBase.cs
@@ -0,0 +1,115 @@
+﻿using MCB.Core.Domain.Entities.Abstractions;
+using MCB.Core.Domain.Entities.Abstractions.ValueObjects;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("MCB.Core.Domain.Entities.Tests")]
+namespace MCB.Core.Domain.Entities
+{
+    public abstract class DomainEntityBase
+        : IDomainEntity
+    {
+        // Properties
+        public Guid Id { get; private set; }
+        public Guid TenantId { get; private set; }
+        public AuditableInfoValueObject AuditableInfo { get; private set; }
+        public DateTimeOffset RegistryVersion { get; private set; }
+
+        // Constructors
+        protected internal DomainEntityBase()
+        {
+            AuditableInfo = new AuditableInfoValueObject();
+        }
+
+        // Private Methods
+        private DomainEntityBase SetId(Guid id)
+        {
+            Id = id;
+            return this;
+        }
+        private DomainEntityBase GenerateNewId() => SetId(Guid.NewGuid());
+        private DomainEntityBase SetTenant(Guid tenantId)
+        {
+            TenantId = tenantId;
+            return this;
+        }
+        private DomainEntityBase SetAuditableInfo(
+            string createdBy,
+            DateTimeOffset createdAt,
+            string? updatedBy,
+            DateTimeOffset? updatedAt,
+            string sourcePlatform
+        )
+        {
+            AuditableInfo = new AuditableInfoValueObject(
+                createdBy,
+                createdAt,
+                updatedBy,
+                updatedAt,
+                sourcePlatform
+            );
+
+            return this;
+        }
+        private DomainEntityBase SetRegistryVersion(DateTimeOffset registryVersion)
+        {
+            RegistryVersion = registryVersion;
+            return this;
+        }
+        private DomainEntityBase GenerateNewRegistryVersion() => SetRegistryVersion(DateTimeOffset.UtcNow);
+
+        // Protected Internal Methods
+        protected internal DomainEntityBase RegisterNew(
+            Guid tenantId,
+            string executionUser,
+            string sourcePlatform
+        )
+        {
+            return GenerateNewId()
+                .SetTenant(tenantId)
+                .SetAuditableInfo(
+                    createdBy: executionUser,
+                    createdAt: DateTimeOffset.UtcNow,
+                    updatedBy: null,
+                    updatedAt: null,
+                    sourcePlatform
+                )
+                .GenerateNewRegistryVersion();
+        }
+        protected internal DomainEntityBase SetExistingInfo(
+            Guid id,
+            Guid tenantId,
+            string createdBy,
+            DateTimeOffset createdAt,
+            string updatedBy,
+            DateTimeOffset? updatedAt,
+            string sourcePlatform,
+            DateTimeOffset registryVersion
+        )
+        {
+            return SetId(id)
+                .SetTenant(tenantId)
+                .SetAuditableInfo(
+                    createdBy,
+                    createdAt,
+                    updatedBy,
+                    updatedAt,
+                    sourcePlatform
+                )
+                .SetRegistryVersion(registryVersion);
+        }
+        protected internal DomainEntityBase RegisterModification(
+            string executionUser,
+            string sourcePlatform
+        )
+        {
+            return SetAuditableInfo(
+                createdBy: AuditableInfo.CreatedBy,
+                createdAt: AuditableInfo.CreatedAt,
+                updatedBy: executionUser,
+                updatedAt: DateTimeOffset.UtcNow,
+                sourcePlatform
+            )
+            .GenerateNewRegistryVersion();
+        }
+    }
+}
---
+++ tst/MCB.Core.Domain.Entities.Tests/DomainEntityBaseTest.cs
@@ -0,0 +1,101 @@
+﻿using FluentAssertions;
+using System;
+using Xunit;
+
+namespace MCB.Core.Domain.Entities.Tests
+{
+    public class DomainEntityBaseTest
+    {
+        [Fact]
+        public void DomainEntityBase_Should_RegisterNew()
+        {
+            // Arrange
+            var customer = new Customer();
+            var tenantId = Guid.NewGuid();
+            var executionUser = "marcelo.castelo@outlook.com";
+            var sourcePlatform = "AppDemo";
+            var initialCreatedAt = customer.AuditableInfo.CreatedAt;
+            var initialRegistryVersion = customer.RegistryVersion;
+
+            // Act
+            customer.RegisterNew(tenantId, executionUser, sourcePlatform);
+
+            // Assert
+            customer.Id.Should().NotBe(default(Guid));
+            customer.TenantId.Should().Be(tenantId);
+            customer.AuditableInfo.CreatedBy.Should().Be(executionUser);
+            customer.AuditableInfo.CreatedAt.Should().BeAfter(initialCreatedAt);
+            customer.AuditableInfo.UpdatedBy.Should().BeNull();
+            customer.AuditableInfo.UpdatedAt.Should().BeNull();
+            customer.AuditableInfo.SourcePlatform.Should().Be(sourcePlatform);
+            customer.RegistryVersion.Should().BeAfter(initialRegistryVersion);
+        }
+
+        [Fact]
+        public void DomainEntityBase_Should_SetExistingInfo()
+        {
+            // Arrange
+            var customer = new Customer();
+            var id = Guid.NewGuid();
+            var tenantId = Guid.NewGuid();
+            var createdBy = "marcelo.castelo@outlook.com";
+            var createdAt = DateTimeOffset.UtcNow;
+            var updatedBy = "marcelo.castelo@github.com";
+            var updatedAt = DateTimeOffset.UtcNow;
+            var sourcePlatform = "AppDemo";
+            var registryVersion = DateTimeOffset.UtcNow;
+
+            // Act
+            customer.SetExistingInfo(id, tenantId, createdBy, createdAt, updatedBy, updatedAt, sourcePlatform, registryVersion);
+
+            // Assert
+            customer.Id.Should().Be(id);
+            customer.TenantId.Should().Be(tenantId);
+            customer.AuditableInfo.CreatedBy.Should().Be(createdBy);
+            customer.AuditableInfo.CreatedAt.Should().Be(createdAt);
+            customer.AuditableInfo.UpdatedBy.Should().Be(updatedBy);
+            customer.AuditableInfo.UpdatedAt.Should().Be(updatedAt);
+            customer.AuditableInfo.SourcePlatform.Should().Be(sourcePlatform);
+            customer.RegistryVersion.Should().Be(registryVersion);
+        }
+
+        [Fact]
+        public void DomainEntityBase_Should_RegisterModification()
+        {
+            // Arrange
+            var customer = new Customer();
+            var tenantId = Guid.NewGuid();
+            var executionUser = "marcelo.castelo@outlook.com";
+            var sourcePlatform = "AppDemo";
+            customer.RegisterNew(tenantId, executionUser, sourcePlatform);
+
+            var initialId = customer.Id;
+            var initialCreatedBy = customer.AuditableInfo.CreatedBy;
+            var initialCreatedAt = customer.AuditableInfo.CreatedAt;
+            var initialUpdatedAt = customer.AuditableInfo.UpdatedAt;
+            var initialRegistryVersion = customer.RegistryVersion;
+
+            var modificationExecutionUser = "marcelo.castelo@github.com";
+            var modificationSourcePlatform = "AppDemo2";
+
+            // Act
+            customer.RegisterModification(modificationExecutionUser, modificationSourcePlatform);
+
+            // Assert
+            customer.Id.Should().Be(initialId);
+            customer.TenantId.Should().Be(tenantId);
+            customer.AuditableInfo.CreatedBy.Should().Be(initialCreatedBy);
+            customer.AuditableInfo.CreatedAt.Should().Be(initialCreatedAt);
+            customer.AuditableInfo.UpdatedBy.Should().Be(modificationExecutionUser);
+            customer.AuditableInfo.UpdatedAt.Should().BeAfter(initialUpdatedAt ?? default);
+            customer.AuditableInfo.SourcePlatform.Should().Be(modificationSourcePlatform);
+            customer.RegistryVersion.Should().BeAfter(initialRegistryVersion);
+        }
+    }
+
+    public class Customer
+        : DomainEntityBase
+    {
+
+    }
+}
