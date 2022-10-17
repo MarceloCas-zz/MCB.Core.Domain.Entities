9f29ae085ff1e888ed9d505316bb267a68403552
Marcelo Castelo Branco <marcelo.castelo@outlook.com>
2022-10-17 01:39:13 -0300

    feat: add domain event base

---
+++ src/MCB.Core.Domain.Entities/DomainEntitiesBase/Events/DomainEventBase.cs
@@ -0,0 +1,29 @@
+﻿using MCB.Core.Domain.Entities.Abstractions;
+using MCB.Core.Domain.Entities.Abstractions.DomainEvents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCB.Core.Domain.Entities.DomainEntitiesBase.Events
+{
+    public abstract record DomainEventBase
+        : IDomainEvent
+    {
+        // Public Methods
+        public Guid Id { get; }
+        public DateTimeOffset Timestamp { get; }
+        public string DomainEventType { get; }
+        public IAggregationRoot AggregationRoot { get; }
+
+        // Constructors
+        protected DomainEventBase(Guid id, DateTimeOffset timestamp, string domainEventType, IAggregationRoot aggregationRoot)
+        {
+            Id = id;
+            Timestamp = timestamp;
+            DomainEventType = domainEventType;
+            AggregationRoot = aggregationRoot;
+        }
+    }
+}
